7e3ec8ff8134537dcca71dcec50811af7fac45fa
nikhil <nikhilagarwal32@gmail.com>
2018-11-23 18:32:56 -0700

    considering all models

--- code/task6a.py
+++ code/task6a.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import constants
 from data_extractor import DataExtractor
+from sklearn.preprocessing import MinMaxScaler
 import pickle
 
 class Task6a():
@@ -17,20 +18,15 @@ class Task6a():
 
 	def generate_img_img_adj_matrix(self):
 		""" Method: generate image-image similarity matrix and stash in pickle file"""
-		print("computing adj matrix...")
+		print("getting and normalizing data...")
 		data_extractor = DataExtractor()
 		loc_mapping = data_extractor.location_mapping()
-		self.img_feature_matrix = data_extractor.prepare_dataset_for_task1(loc_mapping)
+		self.img_feature_matrix = data_extractor.prepare_dataset_for_task6(loc_mapping)
+		scaler = MinMaxScaler()
+		scaler.fit(list(self.img_feature_matrix.values()))
+		for img, feature in self.img_feature_matrix.items():
+			self.img_feature_matrix[img] = scaler.transform([feature])[0]
 		self.img_ids = list(self.img_feature_matrix.keys())
-		# for id1 in self.img_ids:
-		# 	img_matrix = []
-		# 	for id2 in self.img_ids:
-		# 		img_matrix.append(self.get_euclidean_similarity(self.img_feature_matrix[id1], self.img_feature_matrix[id2]))
-		# 	self.adj_matrix.append(img_matrix)
-		# print('pickling the adj matrix...')
-		# with open('adj_mat_6a.pickle', 'wb') as file_handle:
-		# 	pickle.dump(self.adj_matrix, file_handle, protocol=pickle.HIGHEST_PROTOCOL)
-		# print(np.shape(np.asarray(adj_matrix)))
 
 	def get_img_img_adj_matrix(self):
 		""" Method: read image-image similarity matrix from pickle file"""
@@ -49,12 +45,11 @@ class Task6a():
 		self.input_image_label_pairs = OrderedDict(zip(input_images, input_labels))
 		for i in range(0, len(input_labels)):
 			if input_labels[i] in self.label_img_matrix:
-				self.label_img_matrix[input_labels[i]].append(input_images[i])
+				self.label_img_matrix[input_labels[i]].append([input_images[i],0])
 			else:
-				self.label_img_matrix[input_labels[i]] = [input_images[i]]
-		print('initial fianl matrix ', self.label_img_matrix)
+				self.label_img_matrix[input_labels[i]] = [[input_images[i],0]]
 
-	def classify_images(self):
+	def classify_images(self, k):
 		""" Method: Classify all images based on given image-label pairs"""
 		labelled_image_ids = list(self.input_image_label_pairs.keys())
 		input_image_labels = list(self.input_image_label_pairs.values())
@@ -62,52 +57,33 @@ class Task6a():
 			label_similarity_dict = dict()
 			for i, labelled_image in enumerate(labelled_image_ids):
 				if input_image_labels[i] in label_similarity_dict:
-					label_similarity_dict[input_image_labels[i]] += self.get_euclidean_similarity(self.img_feature_matrix[image], self.img_feature_matrix[str(labelled_image)])
+					label_similarity_dict[input_image_labels[i]] += self.get_euclidean_similarity(self.img_feature_matrix[str(image)], self.img_feature_matrix[str(labelled_image)])
 				else:
-					label_similarity_dict[input_image_labels[i]] = self.get_euclidean_similarity(self.img_feature_matrix[image], self.img_feature_matrix[str(labelled_image)])
+					label_similarity_dict[input_image_labels[i]] = self.get_euclidean_similarity(self.img_feature_matrix[str(image)], self.img_feature_matrix[str(labelled_image)])
 			for label in label_similarity_dict:
 				label_similarity_dict[label] = label_similarity_dict[label]/input_image_labels.count(label)
 			min_sim = min(label_similarity_dict.values())
 			for label, sim in label_similarity_dict.items():
 				if sim == min_sim:
-					self.label_img_matrix[label].append(image)
+					self.label_img_matrix[label].append([image, sim])
 
 	def pretty_print(self):
+		""" Method: Print all image label pairs onto html file for visualization"""
 		op = open(constants.TASK6a_OUTPUT_FILE, "w")
-
+		image_ids_seen = []
 		for label, image_ids in self.label_img_matrix.items():
 			op.write("Label " + label + "\n")
+			image_ids.sort(key=lambda x: x[1])
 			for temp in image_ids:
-				op.write(str(temp) + "\n")
+				if temp[0] not in image_ids_seen:
+					op.write(str(temp[0]) + "\n")
+					image_ids_seen.append(temp[0])
 			op.write("####\n")
-
-			# # similarity_image_dict = Dict(zip(similarity_matrix, input_image_labels))
-			# image_similarity_dict = Dict(zip(input_image_labels, similarity_matrix))
-			# label_sum_dict = dict()
-			# min_sum = 999999999999
-			# min_label = ''
-			# for label in image_similarity_dict.keys():
-			# 	sum_val = sum(image_similarity_dict[label])
-			# 	label_sum_dict[label] = sum_val
-			# 	if min_sum > sum_val:
-			# 		min_sum = sum_val
-			# 		min_label = label
-			# 	else:
-
-
-		pass
-
 		
 if __name__ == "__main__":
 	task = Task6a()
+	k = input('Enter value of k:')
 	task.generate_img_img_adj_matrix()
-	if '40222616' in task.img_feature_matrix:
-		print('found')
-	else:
-		print('not found')
-	# task.get_img_img_adj_matrix()
-
-	# test this code..
 	task.read_input_labels_pairs()
-	task.classify_images()
+	task.classify_images(k)
 	task.pretty_print()
